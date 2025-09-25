cd6ff80de5cb64f87cdbbe6631ac96d3a03b7bcc
KremitTheForg <orlichj@outlook.com>
2025-09-25 10:53:57 +1000

    Show uploaded resumes in admin candidate list

--- backend/app/routers/admin/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Admin-facing HTML views grouped by domain."""
-
-from .router import router  # noqa: F401
-from . import applicants  # noqa: F401
-from . import dashboard  # noqa: F401
-from . import users  # noqa: F401
-
-__all__ = ["router"]
